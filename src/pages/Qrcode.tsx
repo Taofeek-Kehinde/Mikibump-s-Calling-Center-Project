e0d9691b722f9e7904f1a32766af45ef68626010
Taofeek Kehinde <Ktaofeek015@gmal.com>
2026-02-16 12:40:18 +0000

    Fix css filename case

--- src/pages/Qrcode.tsx
+++ src/pages/Qrcode.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { QRCodeCanvas } from "qrcode.react";
 import { v4 as uuidv4 } from "uuid";
 import "./Qrcode.css";
-
 export default function Qrcode() {
   const [batchId, setBatchId] = useState("");
   const [qrUrl, setQrUrl] = useState("");
--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import "./view.css";
+import "./View.css";
 
 export default function View() {
   const { id } = useParams();
